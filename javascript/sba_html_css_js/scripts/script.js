f38fc3a1df8b5a7ae240e6551c9f41027783f351
DustyHatz <dustyhatz21@mac.com>
2021-02-12 13:54:54 -0600

    safety save

--- javascript/sba_html_css_js/scripts/script.js
+++ javascript/sba_html_css_js/scripts/script.js
@@ -17,6 +17,36 @@ function userSearch() {
     }
 }
 
+function validateForm(){
+    var nameRegex = /^[a-zA-Z\-]+$/;
+    var validUsername = document.getElementById("username").value.match(nameRegex);
+    if(validUsername == null){
+        alert("Your username is not valid. Only characters A-Z or a-z or -...");
+        document.frm.username.focus();
+        return false;
+    } else {
+        return true;
+    }
+}
+
 function userLogin() {
 
+    let user_username = document.getElementById("username").value;
+    let user_password = document.getElementById("password").value;
+
+    if (user_username == "" || user_password == "") {
+        return alert("Please don't leave username or password blank...");
+    } else {
+        if (validateForm() == true){
+            return window.location.href = "profile.html";
+        }
+    }
+}
+
+function focusMethodHome() {
+    return document.getElementById("search-input").focus();
+}
+
+function focusMethodLogin() {
+    return document.getElementById("username").focus();
 }
