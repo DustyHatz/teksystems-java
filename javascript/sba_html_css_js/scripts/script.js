643e37c1c35c25fca5497f11f10566cbdf3fdfaa
DustyHatz <dustyhatz21@mac.com>
2021-02-12 13:27:51 -0600

    change search for loop to include method

--- javascript/sba_html_css_js/scripts/script.js
+++ javascript/sba_html_css_js/scripts/script.js
@@ -3,19 +3,20 @@ let whiskeys = ["Ardbeg", "Laphroaig", "Lagavulin"];
 function userSearch() {
 
     let user_search = document.getElementById("search-input").value;
-    //console.log(typeof(user_search));
 
     if (user_search == "") {
         return alert("Please enter the name of a whiskey...");
-
-    }
-
-    for (let whiskey in whiskeys) {
-        if (whiskey != user_search) {
-            return alert("Sorry, whiskey not found in our database...");
-        } else {
+    } else {
+        if (whiskeys.includes(user_search)) {
             console.log(true);
+            //document.getElementById("whiskey-pic")
+            return window.location.href = "whiskey.html";
+        } else {
+            alert("Sorry, whiskey not found in our database...");
         }
     }
-    
+}
+
+function userLogin() {
+
 }
