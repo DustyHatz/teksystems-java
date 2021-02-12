fa2f0a1e478ba2dc90126be6ba2891eaa2b32acb
DustyHatz <dustyhatz21@mac.com>
2021-02-12 16:35:29 -0600

    make all final comments and clean up formatting

--- javascript/sba_html_css_js/README.md
+++ javascript/sba_html_css_js/README.md
@@ -5,11 +5,14 @@
     - Validates a username and password
     - Takes user to profile page
 - Search Form
-    - Searches a list of whiskeys
+    - Searches a list of whiskeys (for now it only matches "Ardbeg")
     - If search is a match, takes user to whiskey information page
 
 ## Profile Page
-- Displays user's name, photo, and list of favorite whiskeys
+- Displays user photo, info, and list of favorite whiskeys
 
 ## Whiskey Info Page 
-- Displays the whiskey's name, type, and ABV.
+- Displays the whiskey's name, country, region, type, and similar whiskey
+
+## Contact Page
+- Contact information about the site creator...in a table format!
--- javascript/sba_html_css_js/scripts/script.js
+++ javascript/sba_html_css_js/scripts/script.js
@@ -1,15 +1,15 @@
-let whiskeys = ["Ardbeg", "Laphroaig", "Lagavulin"];
+// Create mock DB of whiskey
+let whiskeys = ["ardbeg", "laphroaig", "lagavulin"];
 
+// Function to search whiskeys and load the whiskey page if match
 function userSearch() {
 
-    let user_search = document.getElementById("search-input").value;
+    let user_search = document.getElementById("search-input").value.toLowerCase();
 
     if (user_search == "") {
         return alert("Please enter the name of a whiskey...");
     } else {
         if (whiskeys.includes(user_search)) {
-            console.log(true);
-            //document.getElementById("whiskey-pic")
             return window.location.href = "whiskey.html";
         } else {
             alert("Sorry, whiskey not found in our database...");
@@ -17,10 +17,12 @@ function userSearch() {
     }
 }
 
-function validateForm(){
+// Function to validate username with regex
+function validateForm() {
+
     var nameRegex = /^[a-zA-Z\-]+$/;
     var validUsername = document.getElementById("username").value.match(nameRegex);
-    if(validUsername == null){
+    if (validUsername == null) {
         alert("Your username is not valid. Only characters A-Z or a-z or -...");
         document.frm.username.focus();
         return false;
@@ -29,6 +31,7 @@ function validateForm(){
     }
 }
 
+// Function to validate username and password fields and take user to profile page
 function userLogin() {
 
     let user_username = document.getElementById("username").value;
@@ -37,12 +40,13 @@ function userLogin() {
     if (user_username == "" || user_password == "") {
         return alert("Please don't leave username or password blank...");
     } else {
-        if (validateForm() == true){
+        if (validateForm() == true) {
             return window.location.href = "profile.html";
         }
     }
 }
 
+// Functions to focus on the input boxes of either search or login form
 function focusMethodHome() {
     return document.getElementById("search-input").focus();
 }
