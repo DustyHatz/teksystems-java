e99efeef10503e6ebc6038c005691fc50628c5a8
DustyHatz <dustyhatz21@mac.com>
2021-02-23 16:05:47 -0600

    add completed solution for query 10

---
+++ sql/sql_sba/sba_query_10.sql
@@ -0,0 +1,15 @@
+SELECT 
+  course.name AS Course_Name, 
+  ROUND(
+    AVG(studentcourse.progress), 
+    1
+  ) AS Average_Progress 
+FROM 
+  studentcourse 
+  JOIN course ON studentcourse.courseId = course.id 
+GROUP BY 
+  studentcourse.courseId 
+ORDER BY 
+  Average_Progress DESC
+LIMIT 
+  1;
