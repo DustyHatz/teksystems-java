3678d7ecac6b520fe3240778d84caa220687ad48
DustyHatz <dustyhatz21@mac.com>
2021-02-23 14:49:34 -0600

    add completed solution for query 7

--- sql/sql_sba/sba_query_7.sql
+++ sql/sql_sba/sba_query_7.sql
@@ -1,5 +1,10 @@
-SELECT *
+SELECT
+student.firstname AS First_Name,
+student.lastname AS Last_Name,
+COUNT(studentcourse.studentId) AS Number_of_Courses
 FROM studentcourse
 LEFT JOIN course ON studentcourse.courseId = course.id
 LEFT JOIN student ON student.id = studentcourse.studentId
-WHERE student.majorId = course.deptId;
+WHERE student.majorId = course.deptId
+GROUP BY studentcourse.studentId
+ORDER BY Number_of_Courses DESC, First_Name, Last_Name;
