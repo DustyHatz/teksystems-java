472dee11ffbee27ad68d4bfb3c4add8747e1968f
DustyHatz <dustyhatz21@mac.com>
2021-02-24 14:00:35 -0600

    add directory for week 5 java exercises and first solution

---
+++ java/CoreJavaBasics/src/com/perscholas/java_basics/JavaBasicsClass.java
@@ -0,0 +1,28 @@
+package com.perscholas.java_basics;
+
+public class JavaBasicsClass {
+
+	public static void main(String[] args) {
+		
+		addInt();
+
+	}
+	
+	
+	private static void addInt() {
+
+		int a;
+		int b;
+		
+		a = 5;
+		b = 1;
+		
+		int result = a + b;
+		
+		System.out.println(result);
+		
+	}
+	
+	
+		
+}
