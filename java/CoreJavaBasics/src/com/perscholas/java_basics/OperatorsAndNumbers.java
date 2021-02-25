ec535bdda6e17bd075ea0a04e5e38e2b1156ce09
DustyHatz <dustyhatz21@mac.com>
2021-02-25 16:15:25 -0600

    add solution for #3

--- java/CoreJavaBasics/src/com/perscholas/java_basics/OperatorsAndNumbers.java
+++ java/CoreJavaBasics/src/com/perscholas/java_basics/OperatorsAndNumbers.java
@@ -3,18 +3,15 @@ package com.perscholas.java_basics;
 public class OperatorsAndNumbers extends JavaBasicsClass {
 
 	public static void main(String[] args) {
+		
+		intBinaryShiftOne();
 
 		/*
+		 * #1
 		 * Write the following integers in binary notation. Do not use any Java
 		 * functions or online conversion applications to calculate the answer as this
 		 * will hinder the learning process and your understanding of the concept.
-		 * However, you may check your answers using Java methods. 
-		 * 1 
-		 * 8 
-		 * 33 
-		 * 78 
-		 * 787 
-		 * 33,987
+		 * However, you may check your answers using Java methods. 1 8 33 78 787 33,987
 		 */
 
 		// 1 --> 0000 0001
@@ -24,28 +21,64 @@ public class OperatorsAndNumbers extends JavaBasicsClass {
 		// 787 --> 0011 0001 0011
 		// 33,987 --> 0100 0010 0110 00011
 
-/*--------------------------------------------------------------------------------------------------------*/
-		
+		/*--------------------------------------------------------------------------------------------------------*/
+
 		/*
-		 * Convert the following binary numbers to decimal notation. Do not use any Java
-		 * functions or online conversion applications to calculate the answer as this
-		 * will hinder the learning process and your understanding of the concept.
-		 * However, you may check your answers using Java methods. 
-		 * 0010
-		 * 1001
-		 * 0011 0100
-		 * 0111 0010
-		 * 0010 0001 1111
-		 * 0010 1100 0110 0111
+		 * #2
+		 * Convert the following binary numbers to decimal notation. Do not use any
+		 * Java functions or online conversion applications to calculate the answer as
+		 * this will hinder the learning process and your understanding of the concept.
+		 * However, you may check your answers using Java methods. 0010 1001 0011 0100
+		 * 0111 0010 0010 0001 1111 0010 1100 0110 0111
+		 */
+
+		// 0010 --> 2
+		// 1001 --> 9
+		// 0011 0100 --> 52
+		// 0111 0010 --> 114
+		// 0010 0001 1111 --> 543
+		// 0010 1100 0110 0111 --> 11,367
+		
+		/*--------------------------------------------------------------------------------------------------------*/
+	
+		/* #3
+		 * Write a program that declares an integer a variable x and assigns the value 2
+		 * to it and prints out the binary string version of the number (
+		 * Integer.toBinaryString(x) ). Now, use the left shift operator (<<) to shift
+		 * by 1 and assign the result to x. Before printing the results, write a comment
+		 * with the predicted decimal value and binary string. Now, print out x in
+		 * decimal form and in binary notation. Do the preceding exercise with the
+		 * following values:
+		 * 
+		 * 9 17 88
 		 */
 		
-		 // 0010 --> 2
-		 // 1001 --> 9
-		 // 0011 0100 --> 52
-		 // 0111 0010 --> 114
-		 // 0010 0001 1111 --> 543
-		 // 0010 1100 0110 0111 --> 11,367
+
+	}
+	
+	public static void intBinaryShiftOne() {
+		
+		int x = 2;
+		String xBin = Integer.toBinaryString(x);
+		
+		System.out.println(xBin); // 0010
+		System.out.println(x); // 2
+		
+		x = x << 1;
+		xBin = Integer.toBinaryString(x);
+		
+		System.out.println(xBin); // 0100
+		System.out.println(x); // 4
+		
+		x = 9; // 1001
+		x = x << 1;
+		xBin = Integer.toBinaryString(x);
+		
+		System.out.println(xBin); // 0001 0010
+		System.out.println(x); // 18
+		
 		
 	}
+	
 
 }
