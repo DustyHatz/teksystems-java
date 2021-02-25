fe757e479991db3cede762423026d54295138e84
DustyHatz <dustyhatz21@mac.com>
2021-02-25 11:26:23 -0600

    add operators and numbers exercise and solutions for #1 and #2

---
+++ java/CoreJavaBasics/src/com/perscholas/java_basics/OperatorsAndNumbers.java
@@ -0,0 +1,51 @@
+package com.perscholas.java_basics;
+
+public class OperatorsAndNumbers extends JavaBasicsClass {
+
+	public static void main(String[] args) {
+
+		/*
+		 * Write the following integers in binary notation. Do not use any Java
+		 * functions or online conversion applications to calculate the answer as this
+		 * will hinder the learning process and your understanding of the concept.
+		 * However, you may check your answers using Java methods. 
+		 * 1 
+		 * 8 
+		 * 33 
+		 * 78 
+		 * 787 
+		 * 33,987
+		 */
+
+		// 1 --> 0000 0001
+		// 8 --> 0000 1000
+		// 33 --> 0010 0001
+		// 78 --> 0100 1110
+		// 787 --> 0011 0001 0011
+		// 33,987 --> 0100 0010 0110 00011
+
+/*--------------------------------------------------------------------------------------------------------*/
+		
+		/*
+		 * Convert the following binary numbers to decimal notation. Do not use any Java
+		 * functions or online conversion applications to calculate the answer as this
+		 * will hinder the learning process and your understanding of the concept.
+		 * However, you may check your answers using Java methods. 
+		 * 0010
+		 * 1001
+		 * 0011 0100
+		 * 0111 0010
+		 * 0010 0001 1111
+		 * 0010 1100 0110 0111
+		 */
+		
+		 // 0010 --> 2
+		 // 1001 --> 9
+		 // 0011 0100 --> 52
+		 // 0111 0010 --> 114
+		 // 0010 0001 1111 --> 543
+		 // 0010 1100 0110 0111 --> 11,367
+		
+	}
+
+}
