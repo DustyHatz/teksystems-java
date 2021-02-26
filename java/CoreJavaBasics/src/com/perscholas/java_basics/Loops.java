f88c1727b046626ba1fbd71364efc9de96c2bb30
Dustin Hatzenbuhler <dustyhatz@Dustins-Mac-mini.local>
2021-02-26 15:47:07 -0600

    add solution for #4 multOfFive1

--- java/CoreJavaBasics/src/com/perscholas/java_basics/Loops.java
+++ java/CoreJavaBasics/src/com/perscholas/java_basics/Loops.java
@@ -5,7 +5,8 @@ public class Loops {
 	public static void main(String[] args) {
 		// printTen();
 		// everyTenth();
-		doWhileToTen();
+		// doWhileToTen();
+		multOfFive1();
 	}
 
 	/*
@@ -50,4 +51,34 @@ public class Loops {
 
 	}
 
+	/*
+	 * Write a program that uses a for-loop to loop through the numbers 1-100. Print
+	 * out each number if is a multiple of 5, but do not print out any numbers
+	 * between 25 and 75. Use the “continue” statement to accomplish this.
+	 */
+
+	private static void multOfFive1() {
+
+		for (int i = 1; i <= 100; i++) {
+			
+			if (!(i > 25 && i < 75) && (i % 5 == 0)) {
+				System.out.println(i);
+			}
+		}
+	}
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
