72391c54e1bf6dd0997c0708e43dd1fb3c50a960
Dustin Hatzenbuhler <dustyhatz@Dustins-Mac-mini.local>
2021-02-26 14:41:20 -0600

    add solution for #3 betweenTenAndTwenty

--- java/CoreJavaBasics/src/com/perscholas/java_basics/ControlFlow.java
+++ java/CoreJavaBasics/src/com/perscholas/java_basics/ControlFlow.java
@@ -1,11 +1,15 @@
 package com.perscholas.java_basics;
 
+import java.util.Scanner;
+
 public class ControlFlow {
 
 	public static void main(String[] args) {
 
-		//lessThanTen();
-		lessGreaterThanTen();
+		// lessThanTen();
+		// lessGreaterThanTen();
+		betweenTenAndTwenty();
+
 	}
 
 	/*
@@ -37,7 +41,7 @@ public class ControlFlow {
 	public static void lessGreaterThanTen() {
 
 		int x = 7;
-		//int x = 15;
+		// int x = 15;
 
 		if (x < 10) {
 			System.out.println("Less than 10");
@@ -47,4 +51,28 @@ public class ControlFlow {
 		}
 
 	}
+
+	/*
+	 * Write a program that declares 1 integer variable x, and then assigns 15 to
+	 * it. Write an “if-else-if” statement that prints out “Less than 10” if x is
+	 * less than 10, “Between 10 and 20” if x is greater than 10 but less than 20,
+	 * and “Greater than or equal to 20” if x is greater than or equal to 20. Change
+	 * x to 50 and notice the result.
+	 */
+
+	public static void betweenTenAndTwenty() {
+
+		int x = 15;
+
+		if (x < 10) {
+			System.out.println("Less than 10");
+		} else if (x > 10 && x < 20) {
+			System.out.println("Between 10 and 20");
+		} else {
+			System.out.println("Greater than or equal to 20");
+
+		}
+
+	}
+
 }
