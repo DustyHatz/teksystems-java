852c7c7bc49cd25510129cacfddf5c2481ae58b6
Dustin Hatzenbuhler <dustyhatz@Dustins-Mac-mini.local>
2021-02-26 15:10:12 -0600

    add solution for #6 weekday

--- java/CoreJavaBasics/src/com/perscholas/java_basics/ControlFlow.java
+++ java/CoreJavaBasics/src/com/perscholas/java_basics/ControlFlow.java
@@ -10,7 +10,8 @@ public class ControlFlow {
 		// lessGreaterThanTen();
 		// betweenTenAndTwenty();
 		// inRange();
-		grades();
+		// grades();
+		// weekday();
 
 	}
 
@@ -112,26 +113,23 @@ public class ControlFlow {
 	 * letter grade. Print out “Score out of range.” if the score is less than 0 or
 	 * greater than 100.
 	 */
-	
+
 	public static void grades() {
 		Scanner scn = new Scanner(System.in);
-		
+
 		System.out.println("Enter an integer: ");
-		
+
 		int x = scn.nextInt();
-		
+
 		if (x < 60) {
 			System.out.println('F');
-		}
-		else if (x >= 60 && x <=69) {
+		} else if (x >= 60 && x <= 69) {
 			System.out.println('D');
 
-		}
-		else if (x >= 70 && x <=79) {
+		} else if (x >= 70 && x <= 79) {
 			System.out.println('C');
 
-		}
-		else if (x >= 80 && x <=89) {
+		} else if (x >= 80 && x <= 89) {
 			System.out.println('B');
 
 		} else {
@@ -139,7 +137,52 @@ public class ControlFlow {
 
 		}
 	}
-	
-	
+
+	/*
+	 * Write a program that accepts an integer between 1 and 7 from the user. Use a
+	 * switch statement to print out the corresponding weekday. Print “Out of range”
+	 * if the number is less than 1 or greater than 7. Don’t forget to include
+	 * “break” statements in each of your
+	 */
+
+	public static void weekday() {
+		Scanner scn = new Scanner(System.in);
+
+		int input;
+		do {
+			System.out.print("Enter an integer between 1 and 7: ");
+			input = scn.nextInt();
+
+			if (input < 1 || input > 7) {
+				System.out.println("Out of range!");
+			}
+		} while (input < 1 || input > 7);
+
+		switch (input) {
+
+		case 1:
+			System.out.println("Sunday");
+			break;
+		case 2:
+			System.out.println("Monday");
+			break;
+		case 3:
+			System.out.println("Tuesday");
+			break;
+		case 4:
+			System.out.println("Wednesday");
+			break;
+		case 5:
+			System.out.println("Thursday");
+			break;
+		case 6:
+			System.out.println("Friday");
+			break;
+		case 7:
+			System.out.println("Saturday");
+			break;
+
+		}
+	}
 
 }
