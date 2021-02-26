5c7dca4631947ead892b56900dec564278bd9fe2
Dustin Hatzenbuhler <dustyhatz@Dustins-Mac-mini.local>
2021-02-26 10:21:36 -0600

    add controlFlow class and solution to #1 lessThanTen

---
+++ java/CoreJavaBasics/src/com/perscholas/java_basics/ControlFlow.java
@@ -0,0 +1,28 @@
+package com.perscholas.java_basics;
+
+public class ControlFlow {
+
+	public static void main(String[] args) {
+
+		lessThanTen();
+	}
+
+	/*
+	 * Write a program that declares 1 integer variable x, and then assigns 7 to it.
+	 * Write an if statement to print out “Less than 10” if x is less than 10.
+	 * Change x to equal 15 and notice the result (console should not display
+	 * anything).
+	 */
+
+	public static void lessThanTen() {
+
+		int x = 7;
+		// int x = 15;
+
+		if (x < 10) {
+			System.out.println("Less than 10");
+
+		}
+
+	}
+}
