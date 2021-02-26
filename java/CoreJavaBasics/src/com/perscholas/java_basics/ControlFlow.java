a6ffea8e19dd28068049d8ba5e0374825310d995
Dustin Hatzenbuhler <dustyhatz@Dustins-Mac-mini.local>
2021-02-26 10:29:02 -0600

    add solution for #2 lessGreaterThanTen

--- java/CoreJavaBasics/src/com/perscholas/java_basics/ControlFlow.java
+++ java/CoreJavaBasics/src/com/perscholas/java_basics/ControlFlow.java
@@ -4,7 +4,8 @@ public class ControlFlow {
 
 	public static void main(String[] args) {
 
-		lessThanTen();
+		//lessThanTen();
+		lessGreaterThanTen();
 	}
 
 	/*
@@ -22,6 +23,27 @@ public class ControlFlow {
 		if (x < 10) {
 			System.out.println("Less than 10");
 
+		}
+
+	}
+
+	/*
+	 * Write a program that declares 1 integer variable x, and then assigns 7 to it.
+	 * Write an if-else statement that prints out “Less than 10” if x is less than
+	 * 10 and “Greater than 10” if x is greater than 10. Change x to 15 and notice
+	 * the result.
+	 */
+
+	public static void lessGreaterThanTen() {
+
+		int x = 7;
+		//int x = 15;
+
+		if (x < 10) {
+			System.out.println("Less than 10");
+
+		} else {
+			System.out.println("Greater than 10");
 		}
 
 	}
