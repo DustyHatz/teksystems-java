b0a705672ccdde0b24ff645e17ca83a44b95f3bf
Dustin Hatzenbuhler <dustyhatz@Dustins-Mac-mini.local>
2021-02-26 10:07:46 -0600

    add solution for exercise #8 prefixPostfix

--- java/CoreJavaBasics/src/com/perscholas/java_basics/OperatorsAndNumbers.java
+++ java/CoreJavaBasics/src/com/perscholas/java_basics/OperatorsAndNumbers.java
@@ -8,7 +8,8 @@ public class OperatorsAndNumbers extends JavaBasicsClass {
 		// intBinaryShiftTwo();
 		// bitwiseOperator();
 		// postfixIncrement();
-		multiIncrement();
+		// multiIncrement();
+		prefixPostfix();
 
 		/*
 		 * #1 Write the following integers in binary notation. Do not use any Java
@@ -178,6 +179,32 @@ public class OperatorsAndNumbers extends JavaBasicsClass {
 
 		x += 1;
 		System.out.println(x);
+
+	}
+
+	/*
+	 * Write a program that declares 2 integer variables, x, and y, and then assigns
+	 * 5 to x and 8 to y. Create another variable sum and assign the value of ++x
+	 * added to y and print the result. Notice the value of the sum (should be 14).
+	 * Now change the increment operator to postfix (x++) and re-run the program.
+	 * Notice what the value of sum is. The first configuration incremented x and
+	 * then calculated the sum while the second configuration calculated the sum and
+	 * then incremented x.
+	 */
+	
+	public static void prefixPostfix() {
+
+		int x = 5;
+		int y = 8;
+		
+		int sum = x++ + y;
+		
+		System.out.println(sum);
+		
+		int z = x + 1;
+		
+		System.out.println(z);
+
 
 	}
 
