764f2ea9d60a738320d3640c9bcfd1fbdfac1e95
DustyHatz <dustyhatz21@mac.com>
2021-02-28 21:49:42 -0600

    rename class to avoid import error

--- java/CoreJavaBasics/src/com/perscholas/java_basics/ArraysBasics.java
+++ java/CoreJavaBasics/src/com/perscholas/java_basics/ArraysBasics.java
@@ -1,7 +1,9 @@
 package com.perscholas.java_basics;
 
-public class Arrays {
+import java.util.Arrays;
 
+public class ArraysBasics {
+	
 	public static void main(String[] args) {
 
 		// arrayOfThree();
@@ -11,7 +13,8 @@ public class Arrays {
 		// assignIndexLoop();
 		// assignIndexX2();
 		// notMiddleIndex();
-		swapFirstMiddle();
+		// swapFirstMiddle();
+		sortLargestSmallest();
 	}
 
 	/*
@@ -144,6 +147,24 @@ public class Arrays {
 		for (String word : array) {
 			System.out.println(word);
 		}
+
+	}
+
+	/*
+	 * Write a program to sort the following int array in ascending order: {4, 2, 9,
+	 * 13, 1, 0}. Print the array in ascending order, print the smallest and the
+	 * largest element of the array. The output will look like the following: Array
+	 * in ascending order: 0, 1, 2, 4, 9, 13
+	 * 
+	 * The smallest number is 0
+	 * 
+	 * The biggest number is 13
+	 */
+	private static void sortLargestSmallest() {
+		
+		int[] array = new int[] {4, 2, 9, 13, 1, 0};
+		
+		
 
 	}
 }
