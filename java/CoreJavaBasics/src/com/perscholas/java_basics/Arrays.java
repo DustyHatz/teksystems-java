3fecccd80c429f7344cbf8a898c113945bda2e11
DustyHatz <dustyhatz21@mac.com>
2021-02-28 21:16:34 -0600

    add solution for #4 indexOutOfBounds

--- java/CoreJavaBasics/src/com/perscholas/java_basics/Arrays.java
+++ java/CoreJavaBasics/src/com/perscholas/java_basics/Arrays.java
@@ -6,7 +6,8 @@ public class Arrays {
 
 		// arrayOfThree();
 		// middleElement();
-		cloneArray();
+		// cloneArray();
+		indexOutOfBounds();
 	}
 
 	/*
@@ -53,6 +54,26 @@ public class Arrays {
 		for (String color : array2) {
 			System.out.println(color.toString());
 		}
+	}
+
+	/*
+	 * Write a program that creates an integer array with 5 elements (i.e.,
+	 * numbers). The numbers can be any integers. Print out the value at the first
+	 * index and the last index using length - 1 as the index. Now try printing the
+	 * value at index = length ( e.g., myArray[myArray.length ] ). Notice the type
+	 * of exception which is produced. Now try to assign a value to the array index
+	 * 5. You should get the same type of exception.
+	 */
+
+	private static void indexOutOfBounds() {
+
+		int[] array = new int[] { 1, 2, 3, 4, 5 };
+
+		System.out.println(array[0]);
+		System.out.println(array[array.length - 1]);
+		System.out.println(array[array.length]); // ArrayIndexOutOfBoundsException
+		array[5] = 3; // ArrayIndexOutOfBoundsException
+
 	}
 
 }
