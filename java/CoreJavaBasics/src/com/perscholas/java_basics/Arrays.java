cf40b385e7c5396dd683d9b3c23f6477310b7a97
DustyHatz <dustyhatz21@mac.com>
2021-02-28 21:10:05 -0600

    add solution for #3 cloneArray

--- java/CoreJavaBasics/src/com/perscholas/java_basics/Arrays.java
+++ java/CoreJavaBasics/src/com/perscholas/java_basics/Arrays.java
@@ -5,7 +5,8 @@ public class Arrays {
 	public static void main(String[] args) {
 
 		// arrayOfThree();
-		middleElement();
+		// middleElement();
+		cloneArray();
 	}
 
 	/*
@@ -33,4 +34,25 @@ public class Arrays {
 		System.out.println(array[array.length / 2]);
 
 	}
+
+	/*
+	 * Write a program that creates an array of String type and initializes it with
+	 * the strings “red”, “green”, “blue” and “yellow”. Print out the array length.
+	 * Make a copy using the clone( ) method. Use the Arrays.toString( ) method on
+	 * the new array to verify that the original array was copied.
+	 */
+
+	private static void cloneArray() {
+
+		String[] array = new String[] { "red", "green", "blue", "yellow" };
+
+		System.out.println(array.length);
+
+		String[] array2 = array.clone();
+
+		for (String color : array2) {
+			System.out.println(color.toString());
+		}
+	}
+
 }
