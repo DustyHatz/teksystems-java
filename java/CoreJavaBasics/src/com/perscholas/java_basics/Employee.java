d2cf8fb9c6eb7e183888cc247ac18ca13246235d
Dustin Hatzenbuhler <dustyhatz@Dustins-Mac-mini.local>
2021-03-01 19:15:35 -0600

    add Employee class and constructor method

---
+++ java/CoreJavaBasics/src/com/perscholas/java_basics/Employee.java
@@ -0,0 +1,22 @@
+package com.perscholas.java_basics;
+
+public class Employee {
+
+	long employeeId;
+	String employeeName;
+	String employeeAddress;
+	long employeePhone;
+	double basicSalary;
+	double specialAllowence;
+
+	// Constructor method
+	public Employee(long id, String name, String address, long phone) {
+
+		employeeId = id;
+		employeeName = name;
+		employeeAddress = address;
+		employeePhone = phone;
+
+	}
+
+}
