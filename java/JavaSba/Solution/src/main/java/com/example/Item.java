8461fda6035da3592e7ba6fd3f665ee22b220fb2
Dustin Hatzenbuhler <dustyhatz@Dustins-Mac-mini.local>
2021-03-03 16:31:46 -0600

    initialize constructor input parameters, initialize quantity to 1

--- java/JavaSba/Solution/src/main/java/com/example/Item.java
+++ java/JavaSba/Solution/src/main/java/com/example/Item.java
@@ -14,6 +14,11 @@ public class Item{
     
     public Item(String itemName, String itemDesc, Double itemPrice, Integer availableQuantity) {
         // Your code here
+    	this.itemName = itemName;
+    	this.itemDesc = itemDesc;
+    	this.itemPrice = itemPrice;
+    	this.quantity = 1;
+    	this.availableQuantity = availableQuantity;
     }
     
     
