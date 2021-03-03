e74f831bde25bc136c8fdca3d27bc99b179dffa5
Dustin Hatzenbuhler <dustyhatz@Dustins-Mac-mini.local>
2021-03-03 16:28:29 -0600

    add instance vairables to items class

--- java/JavaSba/Solution/src/main/java/com/example/Item.java
+++ java/JavaSba/Solution/src/main/java/com/example/Item.java
@@ -5,6 +5,11 @@ public class Item{
     
     public Item() {
         // Your code here
+    	private String itemName;
+    	private String itemDesc;
+    	private Double itemPrice;
+    	private Integer quantity;
+    	private Integer availableQuantity;
     }
     
     public Item(String itemName, String itemDesc, Double itemPrice, Integer availableQuantity) {
