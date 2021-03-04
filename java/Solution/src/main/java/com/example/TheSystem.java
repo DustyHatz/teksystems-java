6cd352750535f784f3e943ecc66006cf04e7d23b
Dustin Hatzenbuhler <dustyhatz@Dustins-Mac-mini.local>
2021-03-04 15:58:36 -0600

    add scanner delimiter, read line into variables, build hasmamp

--- java/Solution/src/main/java/com/example/TheSystem.java
+++ java/Solution/src/main/java/com/example/TheSystem.java
@@ -10,39 +10,43 @@ public abstract class TheSystem {
 	private HashMap<String, Item> itemCollection;
 
 	TheSystem() {
-        // Your code here
-    	this.itemCollection = new HashMap<String, Item>();
-    	
-    	if (getClass().getSimpleName().equals("AppSystem")) {
-    		// Add items from sample.txt to the itemCollection
-    		try {
-    			String filePath = "./resources/samples.txt";
-    			File file = new File(filePath);
-    			Scanner scn = new Scanner(file);
-    			
-    			while (scn.hasNextLine()) {
-    				
-    				
-    				// Need to figure out how to read the strings, the double, and integer seperately.
-    				String itemName = scn.next
-    				
-    				String[] itemInfo = scn.nextLine().split("\s ");
-    				
-    				String itemName = itemInfo[0];
-    				String itemDesc = itemInfo[1];
-    				Double itemPrice = itemInfo[2];
-    				Integer availableQuantity = itemInfo[3];
-    				
-    				itemCollection.put(itemName, new Item());
-    				
-    			}
-    			
-    		} catch (FileNotFoundException e) {
-    			System.out.println("FILE NOT FOUND...");
-    		}
-    		
-    	}
-    }
+		// Your code here
+		this.itemCollection = new HashMap<String, Item>();
+
+		if (getClass().getSimpleName().equals("AppSystem")) {
+			// Add items from sample.txt to the itemCollection
+			try {
+				String filePath = "./resources/samples.txt";
+				File file = new File(filePath);
+				Scanner scn = new Scanner(file);
+
+				while (scn.hasNextLine()) {
+
+					// tell Scanner to use double space as the delimiter before reading
+					scn.useDelimiter("  ");
+
+					// scan line for the itemName String, then the itemDesc String, then itemPrice
+					// Double, then availableQuantity Integer
+					String itemName = scn.next();
+					String itemDesc = scn.next();
+					Double itemPrice = scn.nextDouble();
+					Integer availableQuantity = scn.nextInt();
+
+					// enter itemName as key and create a new Item with the above saved variables as
+					// value into the itemCollection
+					itemCollection.put(itemName, new Item(itemName, itemDesc, itemPrice, availableQuantity));
+
+					// scan document for next line
+					scn.nextLine();
+
+				}
+
+			} catch (FileNotFoundException e) {
+				System.out.println("FILE NOT FOUND...");
+			}
+
+		}
+	}
 
 	public HashMap<String, Item> getItemCollection() {
 		// Your code here
