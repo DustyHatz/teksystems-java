c8a9abeae8083f47a823b79761fd3d00b9a546a4
Dustin Hatzenbuhler <dustyhatz@Dustins-Mac-mini.local>
2021-03-08 10:22:11 -0600

    add logic to add method in AppSystem. Successfully passes HR test case #22

--- java/Solution/src/main/java/com/example/AppSystem.java
+++ java/Solution/src/main/java/com/example/AppSystem.java
@@ -1,48 +1,54 @@
 package com.example;
 
 public class AppSystem extends TheSystem {
-    AppSystem() {
-    	
-    }
+	AppSystem() {
 
-    @Override
-    public void display() {
-        // Your code here
-    	System.out.println("AppSystem Inventory:");
+	}
+
+	@Override
+	public void display() {
+		// Your code here
+		System.out.println("AppSystem Inventory:");
 		System.out.printf("%-20s %-20s %-10s %-10s%n", "Name", "Description", "Price", "Available Quantity");
-		
+
 		for (String item : itemCollection.keySet()) {
-			
-			System.out.printf("%-20s %-20s %-10.2f %-10d%n",
-					itemCollection.get(item.toString()).getItemName(),
+
+			System.out.printf("%-20s %-20s %-10.2f %-10d%n", itemCollection.get(item.toString()).getItemName(),
 					itemCollection.get(item.toString()).getItemDesc(),
 					itemCollection.get(item.toString()).getItemPrice(),
 					itemCollection.get(item.toString()).getAvailableQuantity());
 			System.out.println();
 		}
-    	
-    }
 
-    @Override      // this overwrites the parents class add method 
-    public Boolean add(Item item) {
-        // Your code here
-    	return true;
-    }
+	}
 
-    public Item reduceAvailableQuantity(String item_name) {
-        // Your code here
-    	if(!itemCollection.containsKey(item_name)) {
+	@Override // this overwrites the parents class add method
+	public Boolean add(Item item) {
+		// Your code here
+		if (item == null) {
+			return false;
+
+		} else if (itemCollection.containsKey(item.getItemName())) {
+			System.out.println(item.getItemName() + " is already in the App System");
+			return false;
+		}
+		itemCollection.put(item.getItemName(), item);
+		return true;
+	}
+
+	public Item reduceAvailableQuantity(String item_name) {
+		// Your code here
+		if (!itemCollection.containsKey(item_name)) {
+			return null;
+		} else if (itemCollection.get(item_name).getAvailableQuantity() == 0) {
+			itemCollection.remove(item_name);
 			return null;
 		}
-    	else if( itemCollection.get(item_name).getAvailableQuantity() == 0 ) {
-    		itemCollection.remove(item_name);
-    		return null;
-    	}
-    	Item setItem = itemCollection.get(item_name);
-    	setItem.setAvailableQuantity(setItem.getAvailableQuantity() - 1);
-    	itemCollection.remove(item_name);
-    	itemCollection.put(item_name, setItem);
+		Item setItem = itemCollection.get(item_name);
+		setItem.setAvailableQuantity(setItem.getAvailableQuantity() - 1);
+		itemCollection.remove(item_name);
+		itemCollection.put(item_name, setItem);
 		return setItem;
-    }
-    
+	}
+
 }
