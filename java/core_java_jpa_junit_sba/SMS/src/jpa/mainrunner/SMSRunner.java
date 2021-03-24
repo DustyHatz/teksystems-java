5b3d6e5ecf6f077907e64e321806d0bf90c25910
DustyHatz <dustyhatz21@mac.com>
2021-03-23 20:51:44 -0500

    add initial menu system in SMSRunner main

--- java/core_java_jpa_junit_sba/SMS/src/jpa/mainrunner/SMSRunner.java
+++ java/core_java_jpa_junit_sba/SMS/src/jpa/mainrunner/SMSRunner.java
@@ -1,5 +1,7 @@
 package jpa.mainrunner;
 
+import java.util.Scanner;
+
 import jpa.entitymodels.Course;
 import jpa.entitymodels.Student;
 import jpa.service.CourseService;
@@ -11,9 +13,46 @@ public class SMSRunner {
 	public static void main(String[] args) {
 
 		StudentService ss = new StudentService();	
+		CourseService cs = new CourseService();
 		
-		ss.registerStudentToCourse("aiannitti7@is.gd", 1);
+		Scanner sc = new Scanner(System.in);
+		System.out.printf("Are you a(n)%n%n");
+		System.out.printf("%s %n%s %n%s", "1. Student", "2. Quit", "Please enter 1 or 2:");
+		int input = sc.nextInt();
 		
+		switch(input) {
+		
+		case 1:
+			System.out.println("Enter your email: ");
+			String userEmail = sc.next();
+			System.out.println("Enter your password: ");
+			String userPassword = sc.next();
+			if (ss.validateStudent(userEmail, userPassword)) {
+				
+				Student student = ss.getStudentByEmail(userEmail);
+				System.out.printf("Welcome, %s!%n%n", student.getsName());
+				System.out.printf("Your classes:%n%n");
+				// Display Headers
+				System.out.printf("%-20s %-20s %-20s%n", "Course ID", "Course Name", "Instructor Name");
+				
+				for (Course course : student.getsCourses()) {
+					System.out.printf("%-20d, %-20s, %-20s%n%n", course.getCId(), course.getcName(), course.getcInstructorName());
+				}
+				
+				// Display student options menu
+				System.out.printf("1.");
+				
+				
+			} else {
+				System.out.println("Error: Incorrect Username or Password...");
+				break;
+			}
+			
+		case 2:
+			System.out.println("Bye Bye Bye");
+			break;
+		
+		}
 	}
 
 }
